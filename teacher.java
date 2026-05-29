2dbe17a5ac353a6bcc37a802c8cec963f013d320
abellot1621-alt <abellot1621@gmail.com>
2026-05-29 17:04:14 +0300

    Add Teacher class extending Person with displayInfo method

---
+++ teacher.java
@@ -0,0 +1,17 @@
+// Teacher is a Person (Inheritance)
+public class Teacher extends Person {
+    private String department;
+
+    // Constructor
+    public Teacher(String name, String id, String department) {
+        super(name, id);
+        this.department = department;
+    }
+
+    // Overriding (Polymorphism)
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Department: " + department);
+    }
+}

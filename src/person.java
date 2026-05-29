1c38381645422fc56f3b6c0336d4d59397242bdf
abellot1621-alt <abellot1621@gmail.com>
2026-05-29 18:09:01 +0300

    Create person.java

---
+++ src/person.java
@@ -0,0 +1,25 @@
+
+public class Person {
+    private String name;
+    private String id;
+
+    // Constructor
+    public Person(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    // Getters (Encapsulation)
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    // Method to display info (will be overridden)
+    public void displayInfo() {
+        System.out.println("Name: " + name + ", ID: " + id);
+    }
+}
